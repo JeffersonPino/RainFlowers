089d2081a85680abb698e4f657f78a00e74b8b6f
juancho637 <Juancho28@MacBook-Pro-de-juan-2.local>
2015-09-26 18:11:01 -0500

    inicio desarrollo php, login ON

--- js/carpJs/index.js
+++ js/carpJs/index.js
@@ -4,5 +4,34 @@
  * and open the template in the editor.
  */
 $(function (){
-    
+    $.ajax({
+        method: "POST",
+        url: "php/funciones.php",
+        data: {
+            datos:{
+                tipo:"loginConsult"
+            }
+        }
+    }).done(function(msg) {
+        var obj = JSON.parse(msg);
+        if(msg == "null"){
+            $(".liIniciarSesion").append('<a href="login.html">Iniciar Sesion</a>');
+        }else{
+            $(".liIniciarSesion").append(obj[0][1]+' |<button type="button" class="btn btn-link btnStyle btnCerrarSesion">Cerrar Sesión</button>');
+            $(".btnCerrarSesion").off('click');
+            $(".btnCerrarSesion").click(function (){
+                $.ajax({
+                    method: "POST",
+                    url: "php/funciones.php",
+                    data: {
+                        datos:{
+                            tipo:"cerrarSession"
+                        }
+                    }
+                }).done(function() {
+                    window.location="index.html";
+                });
+            });
+        }
+    });
 });
--- js/carpJs/login.js
+++ js/carpJs/login.js
@@ -3,5 +3,26 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
-
+$(function (){
+    $("#btnIniciarSesion").click(function (){
+        var user = $("#user").val();
+        var pass = $("#pass").val();
+        $.ajax({
+            method: "POST",
+            url: "php/funciones.php",
+            data: {
+                datos:{
+                    tipo:"login",
+                    user: user,
+                    pass: pass
+                }
+            }
+        }).done(function(msg) {
+            if(msg == 'null'){
+                alert("Usuario o contraseña incorrectas");
+            }else{
+                window.location="index.html";
+            }
+        });
+    });
+});
